20e5390ea54f537bba281c02e8611619b4a0e3d6
Oluwatosin Adeladan <atosin@gmail.com>
2021-12-22 23:57:01 -0500

    Track client folder

---
+++ client-b/src/main.page/header.js
@@ -0,0 +1,11 @@
+
+const Header = () => (
+    <header className="row">
+        <div className="col-md-12 mt-5 subtitle">
+            Stord interview question. Requirements <a href="./requirements.rtf">here.</a>
+        </div>
+
+    </header>
+);
+
+export default Header;
